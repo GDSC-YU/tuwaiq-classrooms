513b9a9f647d8879ec236e7e8d8a42ec44ea467f
Yazeed AlKhalaf <yazeedfady@gmail.com>
2022-10-29 22:12:01 +0300

    add layout with some of the home screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "pale-blue": "#BBDEFB",
+        "pale-red": "#FFCDD2",
+        "pale-yellow": "#FFECB3",
+        "pale-green": "#C8E6C9",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
+        roboto: ["Roboto", "sans-serif"],
+        cairo: ["Cairo", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
