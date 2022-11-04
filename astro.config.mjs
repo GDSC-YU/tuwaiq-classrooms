d62dca76e8e9a28c3aa3b04063e03187e3e07d78
Yazeed AlKhalaf <yazeedfady@gmail.com>
2022-11-04 22:52:00 +0300

    fix url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "tc.gdscyu.com",
+  site: "http://tc.gdscyu.com",
 });
