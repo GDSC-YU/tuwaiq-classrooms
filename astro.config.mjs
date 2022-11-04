825b63c48ca72763b34b1760d8160560d3667c78
Yazeed AlKhalaf <yazeedfady@gmail.com>
2022-11-04 22:54:54 +0300

    try gihub pages domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "http://tc.gdscyu.com",
+  site: "https://gdsc-yu.github.io",
+  base: "/tuwaiq-classrooms",
 });
