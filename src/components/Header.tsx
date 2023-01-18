64595842e8574b4bb48c20d0a25ecfd48439db5f
Khalil Melhem <118110192+pewpewded@users.noreply.github.com>
2023-01-19 00:00:00 +0300

    Clean up - Added button (#33)
    * clean-up & added back button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import PropTypes from "prop-types";
 import { Children } from "react";
+import { FaArrowLeft } from "react-icons/fa/index.js";
 
 export enum HeaderColor {
   paleYellow = "bg-pale-yellow",
@@ -12,18 +12,24 @@ interface Props {
   title: string;
   color: HeaderColor;
   children?: JSX.Element | JSX.Element[] | string | string[];
+  backButtonHref: string;
 }
 
-export default function Header({ title, color, children }: Props) {
+export default function Header({
+  title,
+  color,
+  children,
+  backButtonHref,
+}: Props) {
   return (
-    <header className={`${color} p-5 rounded-b-[16px]`}>
-      <p className="font-black font-cairo text-4xl">{title}</p>
+    <header className={`${color} rounded-b-[16px] p-5`}>
+      <div className="flex">
+        <a href={backButtonHref} aria-label="Go Back Button">
+          <FaArrowLeft className="text-center text-4xl hover:text-white" />
+        </a>
+        <p className="mx-auto font-cairo text-4xl font-black">{title}</p>
+      </div>
       {Children.map(children, (child) => child)}
     </header>
   );
 }
-
-Header.propTypes = {
-  title: PropTypes.string,
-  color: PropTypes.oneOf(Object.values(HeaderColor)),
-};
