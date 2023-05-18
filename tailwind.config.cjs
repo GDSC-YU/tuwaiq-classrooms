0cb2a0661038d9564ca626e07e335004aa752a76
Khalil Melhem <khalil-melhem@outlook.sa>
2023-05-19 01:58:16 +0300

    index page is done 👍🏻

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        /* yu colors */
         "yu-orange": "#feb150",
         "yu-orange-dark": "#f68939",
+        /* google colors */
+        // dark
+        "google-blue": "#5C9EFF",
+        "google-red": "#F25C4E",
+        "google-yellow": "#FFC247",
+        "google-green": "#33C075",
+        // pale
+        "pale-blue": "#7AB8F5",
+        "pale-red": "#E88383",
+        "pale-yellow": "#FFD476",
+        "pale-green": "#93CFA0",
+        /* social colors */
+        "twitter-blue": "#00acee",
+        "github-purple": "#6e5494",
+        "instagram-pink": "#C13584",
       },
     },
   },
