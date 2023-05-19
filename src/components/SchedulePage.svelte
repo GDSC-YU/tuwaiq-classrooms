4ea4e81c76e9d76c3669967962dbfb5d9f628a6a
Khalil Melhem <khalil-melhem@outlook.sa>
2023-05-19 19:13:48 +0300

    ScheduleLines Component 💯

--- src/components/SchedulePage.svelte
+++ src/components/SchedulePage.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
   import type { Room } from "../data/rooms";
   import { roomTypeMap } from "../data/data";
+
   export let room: Room;
 
   import Header from "../components/ui/Header.svelte";
+  import ScheduleLines from "./schedule/ScheduleLines.svelte";
 
-  const { headerColor, link } = roomTypeMap[room.name.charAt(0)] || {};
+  const { headerColor, link } = roomTypeMap[room.name.charAt(0)];
 </script>
 
 <Header backButtonHref={link} title={room.name} color={headerColor} />
-<div class="flex h-screen items-center justify-center">
-  <h1 class="text-7xl">{room.name} Coming Soon!</h1>
-</div>
+<main>
+  <div class="flex flex-col space-y-12 p-4">
+    <ScheduleLines />
+  </div>
+</main>
---
+++ src/components/schedule/ScheduleLines.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  const times = [
+    "07 am",
+    "08 am",
+    "09 am",
+    "10 am",
+    "11 am",
+    "12 pm",
+    "01 pm",
+    "02 pm",
+    "03 pm",
+    "04 pm",
+    "05 pm",
+    "06 pm",
+  ];
+</script>
+
+{#each times as time}
+  <div class="flex flex-row items-center" aria-hidden="true">
+    <p class="font-bold dark:text-stone-300">{time}</p>
+    <div class="ml-5 h-0 grow border border-slate-900 dark:border-stone-300" />
+  </div>
+{/each}
