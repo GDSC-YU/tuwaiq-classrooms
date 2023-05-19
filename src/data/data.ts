21d6ebb3bc1825078bdb6be2bfa0d62076a7bce0
Khalil Melhem <khalil-melhem@outlook.sa>
2023-05-19 03:35:14 +0300

    new header comp, new color type, general fixes

---
+++ src/data/data.ts
@@ -0,0 +1,6 @@
+export enum GoogleColor {
+  Blue = "bg-pale-blue dark:bg-google-blue",
+  Red = "bg-pale-red dark:bg-google-red",
+  Yellow = "bg-pale-yellow dark:bg-google-yellow",
+  Green = "bg-pale-green dark:bg-google-green",
+}
