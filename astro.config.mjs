02fac32eb046763614bdc56bbbc22373b9fab96f
Khalil Melhem <118110192+pewpewded@users.noreply.github.com>
2023-05-21 22:02:57 +0300

    Gave Tuwaiq Classrooms a Fresh Cut 💯 (#41)
    - Improved Folder Structure 🗂️
    - Dependencies Updated To The Latest Version 👍🏻
    - Refined the design 🎨
      - New DarkMode Toggle 💡
    - General Code Optimizations & Clean Up  🐛🔨
    - New prefetch & sitemap integrations (abused astro 🫣)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
   site: "https://tc.gdscyu.com",
+  integrations: [react(), tailwind(), prefetch(), sitemap()],
 });
