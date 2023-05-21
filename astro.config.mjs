77055bcbef225f11894cb73612ac7019ed5c7153
Khalil Melhem <khalil-melhem@outlook.sa>
2023-05-21 18:51:06 +0300

    generated a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), prefetch()],
+  site: "https://tc.gdscyu.com",
+  integrations: [react(), tailwind(), prefetch(), sitemap()],
 });
